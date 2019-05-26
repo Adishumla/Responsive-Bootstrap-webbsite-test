e6f0dc2d2feb76e6c7b2bbd43dbaae81b8377abd
adam g <adam.garali@gmail.com>
2019-05-26 23:05:23 +0200

    Done

--- Projekt hemsida/script.js
+++ Projekt hemsida/script.js
@@ -1,31 +1,31 @@
-$(document) .ready(function() {
-    $('.nav-button').click(function(){
+$(document).ready(function () {           // Navbar
+    $('.nav-button').click(function () {
         $('.nav-button').toggleClass('change')
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         let position = $(this).scrollTop();
-        if(position >= 200) {
+        if (position >= 200) {
             $('.nav-menu').addClass('custom-navbar');
         } else {
             $('.nav-menu').removeClass('custom-navbar');
         }
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {    // Animation för planet 
         let position = $(this).scrollTop();
-        if(position >= 650) {
-            $('.camera-img').addClass('fromLeft');
+        if (position >= 650) {
+            $('.plane-img').addClass('fromLeft');
             $('.mission-text').addClass('fromRight');
         } else {
-            $('.camera-img').removeClass('fromLeft');
+            $('.plane-img').removeClass('fromLeft');
             $('.mission-text').removeClass('fromRight');
         }
     });
 
-    $('.gallery-list-item').click(function() {
+    $('.gallery-list-item').click(function () {  // animation för galleriet/ travel photos
         let value = $(this).attr('data-filter');
-        if(value === 'all') {
+        if (value === 'all') {
             $('.filter').show(300);
         } else {
             $('.filter').not('.' + value).hide(300);
@@ -33,38 +33,20 @@ $(document) .ready(function() {
         }
     });
 
-    $('.gallary-list-item').click(function() {
+    $('.gallary-list-item').click(function () {
         $(this).addClass('active-item').siblings().removeClass('active-item')
     });
+
+    $(window).scroll(function () {           // animation för priserna
+        let position = $(this).scrollTop();
+        if (position >= 4300) {
+            $('.card-1').addClass('moveFromLeft');
+            $('.card-2').addClass('moveFromBottom');
+            $('.card-3').addClass('moveFromRight');
+        } else {
+            $('.card-1').RemoveClass('moveFromLeft');
+            $('.card-2').RemoveClass('moveFromBottom');
+            $('.card-3').RemoveClass('moveFromRight');
+        }
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
